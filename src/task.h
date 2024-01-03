0288b9f609a5db325a975da9214adcbc3c873765
mikxus <91991837+Mikxus@users.noreply.github.com>
2024-01-03 16:45:22 +0200

    create task struct

--- src/task.h
+++ src/task.h
@@ -1,4 +1,23 @@
 #ifndef _TASK_H_
 #define _TASK_H_
 
-#endif
+#include <cstdint>
+
+/**
+ * @brief Enum for task's possible states
+ */
+typedef enum
+{
+    READY,
+    RUNNING,
+    BLOCKED,
+    SLEEP,
+    UNDEFINED
+} task_state;
+
+struct task_data {
+    uint16_t id;
+    
+    task_state state;
+};
+
