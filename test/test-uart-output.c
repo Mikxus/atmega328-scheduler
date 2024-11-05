9d6194b8ea55a3c08009c25abedef3d387a68abc
Mikxus <91991837+Mikxus@users.noreply.github.com>
2024-11-05 18:15:19 +0200

    chore: add test tools

---
+++ test/test-uart-output.c
@@ -0,0 +1,13 @@
+/* 
+ * test-uart-output.cpp
+ * This file is used to test the uart output
+ * 
+ */
+#include <stddef.h>
+#include "tools/test.h"
+
+int main() {
+    init_avr("uart-output.elf");
+
+    return 0;
+}
