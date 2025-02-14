32ca46994745ae85f63396495940551f4a3ececd
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-02-14 12:36:27 +0200

    fix: printf output
    - fix: uart0 needed to be flushed
    - chore: Remove embedding mcu data in the .elf since we don't use it

--- test/avr/uart-output.cpp
+++ test/avr/uart-output.cpp
@@ -1,23 +1,16 @@
 #include <stdio.h>
 #include <avr/sleep.h>
-#include "submodules/simavr/simavr/sim/avr/avr_mcu_section.h"
 #include "src/scheduler.h"
 
 #define STRINGIZE(x) #x
 #define STRINGIZE_VALUE_OF(x) STRINGIZE(x)
 
-/* 
- * Embed mcu data in the generated ELF file.
- * Useful for the simavr simulator. 
- */
-AVR_MCU(F_CPU, STRINGIZE(SCHEDULER_AVR_MCU));
-
 int main(void)
 {
     _scheduler_init();
-    sei();
-    printf("Hellou!\n");
 
+    printf("Hello World!\n");
+    uart0_flush();
     sleep_cpu();
     return 0;
 }
