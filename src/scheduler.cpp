bc102ca61b340de7d3463bcf572369df000a35a7
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-03-25 07:12:33 +0200

    feat: context_switch functions
    not working at the moment

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -1,11 +1,27 @@
 #include "scheduler.h"
 
 /**
+ * @brief Pointer to the current task
+ */
+volatile struct task_data *c_task = nullptr;
+
+/**
  * @brief Initializes the scheduler before main is called
  */
 void _scheduler_init(void) 
 {
     initialize_uart();
     initialize_clock();
+    return;
+}
+
+// Save the current task's context
+__attribute__((always_inline)) void _save_context(void)
+{
+    volatile struct task_data *task = c_task;
+
+    c_task->cpu_state.sreg = SREG;
+
+
     return;
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -5,7 +5,17 @@
 #include "lib/singly_linked_list/singly_linked_list.h"
 #include "drivers/uart/uart.h" 
 #include "drivers/clock/clock.h"
+#include "kernel/task.h"
+
+/**
+ * @brief Pointer to the current task
+ */
+extern volatile struct task_data *c_task;
+
 
 void _scheduler_init(void);
+
+void _save_context(void);
+void _do_context_switch(void);
 
 #endif
