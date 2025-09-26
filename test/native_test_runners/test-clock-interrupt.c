74b7af7439a3dcd291031a4f30029c130a037c4f
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-09-26 23:13:32 +0300

    fix: finished this test
    - It checks that correct interrupt is ran at ~976 hz

--- test/native_test_runners/test-clock-interrupt.c
+++ test/native_test_runners/test-clock-interrupt.c
@@ -5,69 +5,129 @@
 #include <stddef.h>
 #include <stdio.h> 
 #include <stdlib.h>
+#include <float.h>
+#include <stdbool.h>
 
+/* internal tools */
+#include "tools/debug.h"
 #include "tools/test.h"
+#include "tools/math.h"
+
+/* Simavr */
 #include "sim_elf.h"
 #include "sim_avr.h"
 #include "sim_core.h"
 #include "sim_hex.h"
 #include "avr_timer.h"
 
+#define SAMPLE_SIZE 500
+
 volatile bool interrupt_state = 0;
 
-uint64_t timings_buffer[2900] = {0};
+struct timings_data
+{
+    float min;
+    float max;
+    float avg;
+    float stddev;
+    uint64_t size;
+    float buffer[SAMPLE_SIZE];
+};
 
-static void timer0_comp_cb(struct avr_irq_t *irq, uint32_t value, void *param)
+static void timer0_comp_ovf_cb(struct avr_irq_t *irq, uint32_t value, void *param)
 {
     /*
-     * Check if COMPA triggered the interrupt
+     * Check if TIMER0 overflow triggered the interrupt
      */
-    if (value > 0) return;
+    if (value > 1) return;
 
     interrupt_state = 1;
 }
 
-double convert_cycles_to_frequency(avr_t *avr, uint64_t cycles_delta)
+void calculate_timings(avr_t *avr, struct timings_data *data)
 {
-    double cycles_per_μs = (1.0 / (double) avr->frequency) * 1000000000.0;
-    return 1.0 / ((double) cycles_delta * cycles_per_μs / 1000000000.0);
-}
-
-void analyze_timings(avr_t *avr, uint64_t *timings_buffer, size_t size)
-{
-    uint64_t min = 0;
-    uint64_t max = 0;
-    float avg = 0.0;
-    uint64_t avg_temp = 0; 
+    float avg_sum = 0.0f; 
+    float temp_buff[SAMPLE_SIZE];
+    data->min = FLT_MAX;
+    data->max = FLT_MIN;
 
     /* Calculate differences between readings */
-    for (size_t i = 0; i < size - 1; i++)
+    for (uint64_t i = 0; i < data->size; i++)
     {
         if (i == 0) 
         {
-            min = timings_buffer[i];
+            /* First reading is inaccurate */
             continue;
         }
+        
+        temp_buff[i] = convert_cycles_to_frequency(avr,
+            data->buffer[i] - data->buffer[i - 1]);
 
-        timings_buffer[i] = timings_buffer[i] - timings_buffer[i - 1];
+        if (temp_buff[i] < data->min) data->min = temp_buff[i];
+        if (temp_buff[i] > data->max) data->max = temp_buff[i];
+        avg_sum += temp_buff[i];
+    }
+    /* move our results back */
+    memcpy(&data->buffer[0], &temp_buff[0], sizeof(float) * SAMPLE_SIZE);
 
-        avg_temp += timings_buffer[i];
+    data->avg = avg_sum / (data->size - 1);
 
-        if (timings_buffer[i] < min) min = timings_buffer[i];
-        if (timings_buffer[i] > max) max = timings_buffer[i];
+    /* skip the first reading since it's inaccurate */
+    data->stddev = calculate_standard_deviation(&data->buffer[1], sizeof(data->buffer) / sizeof(float) - 1);
+}
+
+bool verify_timing(struct timings_data *data,
+        float target_avg,
+        float target_min,
+        float target_max,
+        float target_stddev,
+        float accuracy)
+{
+    bool state = 0;
+
+    if (!fequal(data->avg, target_avg, accuracy))
+    {
+        ERROR("AVG: %0.10f | target: %0.10f | accuracy %0.10f differs too much!", data->avg, target_avg, accuracy);
+        state = 1;
+    } else {
+        INFO("AVG: %0.10f | target: %0.10f | accuracy %0.10f  PASS", data->avg, target_avg, accuracy);
     }
 
-    avg = (float) avg_temp / size;
+    if (!fequal(data->min, target_min, accuracy))
+    {
+        ERROR("MIN: %0.10f | target: %0.10f | accuracy %0.10f differs too much!", data->min, target_min, accuracy);
+        state = 1;
+    } else {
+        INFO("MIN: %0.10f | target: %0.10f | accuracy %0.10f  PASS", data->min, target_min, accuracy);
+    }
 
-    INFO("Timings result: ");
-    INFO("MIN: %10.6f", convert_cycles_to_frequency(avr, min));
-    INFO("MAX: %10.6f", convert_cycles_to_frequency(avr, max));
-    INFO("AVG: %10.6f", convert_cycles_to_frequency(avr, avg));
+    if (!fequal(data->max, target_max, accuracy))
+    {
+        ERROR("MAX: %0.10f | target: %0.10f | accuracy %0.10f differs too much!", data->max, target_max, accuracy);
+        state = 1;
+    } else {
+        INFO("MAX: %0.10f | target: %0.10f | accuracy %0.10f  PASS", data->max, target_max, accuracy);
+    }
+
+    if (!fequal(data->stddev, target_stddev, accuracy))
+    {
+        ERROR("STDDEV: %0.10f | target: %0.10f | accuracy %0.10f differs too much!", data->stddev, target_stddev, accuracy);
+        state = 1;
+    } else {
+        INFO("STDDEV: %0.10f | target: %0.10f | accuracy %0.10f  PASS", data->stddev, target_stddev, accuracy);
+    }
+
+    return state;
 }
 
 int main(int argc, char *argv[]) {
 
     avr_t *avr = NULL;
+    struct timings_data timings_data;
+    timings_data.size = SAMPLE_SIZE;
+    timings_data.min = 0.0F;
+    timings_data.max = 0.0F;
+    timings_data.avg = 0.0F;
 
     if (validate_args(argc, argv))
     {
@@ -76,24 +136,35 @@ int main(int argc, char *argv[]) {
 
     avr = init_avr(argv[1], argv[2], atoi(argv[3]));
 
-    /* Register interrupt for TIMER0 COMPARE interrupt
+    /* Register interrupt for TIMER0 COMPARE overflow interrupt
      * our callback function then checks if it was triggered by the timer0 overflow interrupt 
      */
     avr_irq_register_notify(
         avr_io_getirq(avr, AVR_IOCTL_TIMER_GETIRQ('0'), TIMER_IRQ_OUT_COMP),
-        timer0_comp_cb,
+        timer0_comp_ovf_cb,
         NULL
     );
 
-    for (unsigned int i = 0; i < sizeof(timings_buffer) / sizeof(uint64_t); i++)
+    /* collect timing samples */
+    for (unsigned int i = 0; i < timings_data.size; i++)
     {
-        if (!run_avr_until_interrupt(avr, 2000, &interrupt_state))
+        if (run_avr_until_interrupt(avr, 2000, &interrupt_state))
         {
-            interrupt_state = 0;
-            timings_buffer[i] = avr->cycle;
+            ERROR("Test failed avr did not interrupt in time");
+            return 1;
         }
+
+        interrupt_state = 0;
+        timings_data.buffer[i] = avr->cycle;
     }
-    INFO("Analyzing timings");
-    analyze_timings(avr, &timings_buffer[0], sizeof(timings_buffer) / sizeof(uint64_t));
-    return 1;
+
+    calculate_timings(avr, &timings_data);
+
+    /* Verify outputs */
+    return verify_timing(&timings_data,
+        976.57f,
+        976.50f,
+        976.62f,
+        0.02f,
+        0.021f );
 }
