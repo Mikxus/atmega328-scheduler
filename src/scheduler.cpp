9b46af5322e6cae4cdf341b73326ededccda5c67
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-11-22 21:39:08 +0200

    remove _save_context() - Propably shouldn't be in this file

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -14,14 +14,3 @@ void _scheduler_init(void)
     initialize_clock();
     return;
 }
-
-// Save the current task's context
-__attribute__((always_inline)) inline void _save_context(void)
-{
-    volatile struct task_data *task = c_task;
-
-    c_task->cpu_state.sreg = SREG;
-
-
-    return;
-}
