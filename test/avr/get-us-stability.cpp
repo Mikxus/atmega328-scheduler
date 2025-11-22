db2e68f9fef287ab565143ed66aded29db2f97ce
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-11-22 21:38:22 +0200

    feat: add test for get_us() - for verifying it is close enough to correct

---
+++ test/avr/get-us-stability.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <avr/sleep.h>
+#include <avr/io.h>
+#include "src/scheduler.h"
+
+int main(void)
+{
+    _scheduler_init();
+   
+    // Set PB4 as output
+    DDRB = 0b00010000;
+    uint32_t last_us = get_us();           
+    while(true){
+        if (get_us() - last_us >= 1000){
+            PORTB = 0b00010000;
+            last_us = get_us();
+            PORTB ^= 0b00010000;
+        }
+    }
+}
