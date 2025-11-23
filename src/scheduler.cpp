88e938540ea61b389c2aa91e9ca051d22c514614
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-11-23 23:22:08 +0200

    feat: add init for context switch timer - refactor: update c_task protype?

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -3,14 +3,27 @@
 /**
  * @brief Pointer to the current task
  */
-volatile struct task_data *c_task = nullptr;
+volatile task_data_t *c_task = nullptr;
+
+task_data_t init_task;
+
 
 /**
  * @brief Initializes the scheduler before main is called
  */
 void _scheduler_init(void) 
 {
+    cli();
     initialize_uart();
     initialize_clock();
+
+    c_task = &init_task;
+    init_task.burst_time_ms = 1;
+    init_task.state = UNDEFINED;
+
+    initialize_context_switch_timer(1);
+
+    // init 
+    sei();
     return;
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -5,17 +5,19 @@
 #include "lib/singly_linked_list/singly_linked_list.h"
 #include "drivers/uart/uart.h" 
 #include "drivers/clock/clock.h"
-#include "kernel/task.h"
+#include "context_switch.h"
+#include "task.h"
 
 /**
  * @brief Pointer to the current task
  */
-extern volatile struct task_data *c_task;
+extern volatile task_data_t *c_task;
 
+/*
+ * Init task 
+ */
+extern task_data_t init_task;
 
 void _scheduler_init(void);
-
-void _save_context(void);
-void _do_context_switch(void);
 
 #endif
