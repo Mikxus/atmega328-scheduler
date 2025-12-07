f61368358af27fe189b0f15459f14e3fb8b5f28f
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-12-07 09:02:00 +0200

    refactor: move task handling to task.h - seems more logical place

--- src/scheduler.h
+++ src/scheduler.h
@@ -2,16 +2,10 @@
 #define _SCHEDULER_H_
 
 #include <stdio.h>
-#include "lib/singly_linked_list/singly_linked_list.h"
+#include "task.h"
 #include "drivers/uart/uart.h" 
 #include "drivers/clock/clock.h"
 #include "context_switch.h"
-#include "task.h"
-
-/**
- * @brief Pointer to the current task
- */
-extern volatile task_data_t *c_task;
 
 /*
  * Init task 
