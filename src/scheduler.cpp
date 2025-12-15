f77caa4c140959335166cf5940715973912dc1c4
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-12-15 10:42:46 +0200

    refactor: add create_task - feat: add start_scheduler starts preemption and switches context to first task - fixes bug on our way of changing context. Since if not manually changing to first task. On context change would the currently running pc saved as the tasks pc

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -1,31 +1,69 @@
 #include "scheduler.h"
 
-/**
- * @brief Pointer to the current task
- */
-
-task_data_t init_task;
-/**
- * @brief Initializes the scheduler before main is called
- */
-void _scheduler_init(void) 
+void init_scheduler(void) 
 {
+    uint8_t sreg = SREG;
     cli();
     initialize_uart();
     initialize_clock();
 
-    create_task(
-        init_task,
-        (volatile uint8_t *) 0x00,
-        100,
-        "init_task",
-        0,
-        1,
-        nullptr);
-
-    initialize_context_switch_timer(1);
-
-    // init 
-    sei();
+    SREG = sreg;
     return;
+}
+
+void __attribute__((noreturn)) start_scheduler(void)
+{
+    start_context_switch_timer();
+    _RESTORE_CTX();
+    asm volatile ("ret" ::: "memory");
+    __builtin_unreachable();
+}
+
+bool create_task(
+    task_data_t &task,
+    volatile uint8_t *stack_array,
+    const uint16_t stack_size,
+    const char *name,
+    const uint8_t priority,
+    const uint8_t slice_ms,
+    void (entry)(void))
+{
+    uint8_t sreg = SREG;
+    cli();
+
+    #if SCHEDULER_TASK_TRACK_STATISTICS == 1
+    task.priority = priority;
+    task.time_slice_ms = slice_ms;
+    task.exec_time_us = 0;
+    task.exec_time_overflow_count = 0;
+    task.exec_start_time_us = 0;
+    #endif
+    task.next_node = nullptr;
+    task.stack.memory_ptr = stack_array;
+    task.stack.size = stack_size;
+    task.state = READY;
+    task.cpu_state.sreg = 0x80; // interrupts enabled
+
+    task.next_node = nullptr;
+
+    if (strlen(name) >= SCHEDULER_TASK_NAME_MAX_LENGTH)
+        return 1;
+
+    strcpy(task.name, name);
+
+    // initialize stack pointer to third byte? so [0 - 1] can have entry's pc 
+    task.cpu_state.sp = (uint16_t) &stack_array[stack_size - 3];
+
+    // Set entry to stack
+    task.stack.memory_ptr[stack_size - 1] = (uint8_t) ((uint16_t) entry & 0xFF); // pc l 
+    task.stack.memory_ptr[stack_size - 2] = (uint8_t) (((uint16_t) entry >> 8) & 0xFF); // pc h
+
+    _add_task(&task);
+
+    /* if there is no existing task */
+    if (c_task == nullptr)
+        c_task = _get_head_task();
+
+    SREG = sreg;
+    return 0;
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -2,16 +2,42 @@
 #define _SCHEDULER_H_
 
 #include <stdio.h>
+#include <avr/cpufunc.h>
+
 #include "task.h"
+#include "context_switch.h"
 #include "drivers/uart/uart.h" 
 #include "drivers/clock/clock.h"
-#include "context_switch.h"
 
-/*
- * Init task 
+/**
+ * @brief Initializes the scheduler
  */
-extern task_data_t init_task;
+void init_scheduler(void);
 
-void _scheduler_init(void);
+/**
+ * @brief Starts the scheduler
+ * @note Forces context switch to the first task this doesn't return
+ */
+void __attribute__((noreturn)) start_scheduler(void);
+
+/**
+ * @brief Adds a new task to the scheduler
+ * 
+ * @param task 
+ * @param name task name
+ * @param priority task priority (0 - 255)
+ * @param slice_ms lenght of time slice in milliseconds
+ * @param entry Task entry function ptr
+ * @return true 
+ * @return false 
+ */
+bool create_task(
+    task_data_t &task,
+    volatile uint8_t *stack_array,
+    const uint16_t stack_size,
+    const char *name,
+    const uint8_t priority,
+    const uint8_t slice_ms,
+    void (entry)(void));
 
 #endif
