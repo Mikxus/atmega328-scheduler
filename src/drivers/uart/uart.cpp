d3ea067ebbd7501de660bdc32790192059fbb23d
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-12-17 18:31:19 +0200

    refactor: update definition conf name

--- src/drivers/uart/uart.cpp
+++ src/drivers/uart/uart.cpp
@@ -4,7 +4,11 @@ void initialize_uart(void)
 {
     uint8_t sreg = SREG;
     cli();
-    uart_init(BAUD_CALC(SCHEDULER_UART_BAUD_RATE));
+    uart_init(BAUD_CALC(CONF_UART_BAUD_RATE));
+
+    #ifdef USART0_U2X_SPEED
+    uart_set_U2X();
+    #endif
 
     /* Attach UART to stdin and stdout */
     stdout = &uart0_io;
