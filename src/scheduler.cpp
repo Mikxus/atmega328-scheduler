906a3749120a53c28c0acdeb51a369b6876f7440
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-01-09 11:21:48 +0200

    feat: add remove_task

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -52,10 +52,10 @@ bool create_task(
     strcpy(task.name, name);
 
     // initialize stack pointer to third byte? so [0 - 1] can have entry's pc 
-    task.cpu_state.sp = (uint16_t) &stack_array[stack_size - 3];
+    task.cpu_state.sp = (uint16_t) &stack_array[stack_size - 3]; // might be incorrect address
 
     // Set entry to stack
-    task.stack.memory_ptr[stack_size - 1] = (uint8_t) ((uint16_t) entry & 0xFF); // pc l 
+    task.stack.memory_ptr[stack_size - 1] = (uint8_t) ((uint16_t) entry & 0xFF);        // pc l 
     task.stack.memory_ptr[stack_size - 2] = (uint8_t) (((uint16_t) entry >> 8) & 0xFF); // pc h
 
     _add_task(&task);
@@ -68,3 +68,11 @@ bool create_task(
     return 0;
 }
 
+bool remove_task(task_data_t *task)
+{
+    if (_remove_task_from_ready_list(task))
+        return 1;
+
+    _set_task_state(task, UNDEFINED);
+    return 0;
+}
