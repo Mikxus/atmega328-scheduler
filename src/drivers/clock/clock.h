5d8ece3f2233860327dde0c58deae0e533d127fa
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-02-02 21:48:52 +0200

    added comment

--- src/drivers/clock/clock.h
+++ src/drivers/clock/clock.h
@@ -11,8 +11,6 @@
 #include <avr/io.h>
 #include "../timer/timer.h"
 
-
-
 /**
  * @brief Initializes timers needed for the scehduler  
  * @note  Uses timer0's overflow interrupt
@@ -27,6 +25,11 @@ void initialize_clock(void);
  */
 uint32_t get_ms(void);
 
+/**
+ * @brief Returns the uptime in microseconds
+ * @note   
+ * @retval 
+ */
 uint32_t get_us(void);
 
 #endif
