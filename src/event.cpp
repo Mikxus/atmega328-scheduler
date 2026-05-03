16e0a94cbcfdaf525acdfdbcf69aa63d3d5d7643
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-05-03 19:14:19 +0300

    fix: incorrect add_tail call -chore: remove unused code

--- src/event.cpp
+++ src/event.cpp
@@ -6,7 +6,7 @@ kernel_errno_t _add_event(event_t *event)
     if (event == nullptr)
         return KERNEL_ERR_INVALID_PARAMETER;
 
-    _sched_lists.event_list.add_tail(&event);
+    _sched_lists.event_list.add_tail(event);
 
     return KERNEL_OK;
 }
@@ -17,9 +17,4 @@ kernel_errno_t _remove_event(event_t *event)
         return KERNEL_ERR_INVALID_PARAMETER;
 
     return _sched_lists.event_list.remove(event)
-}
-
-bool _is_event_empty(event_t *event)
-{
-    return (event->task_count != 0) ? 1 : 0;
 }
