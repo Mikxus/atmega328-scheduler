614ecc35d244af909158eb48d7b3f4703c7d5082
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-05-11 15:53:00 +0300

    add demo example

---
+++ examples/task-communication/main.cpp
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <avr/pgmspace.h>
+
+#include <kernel/kernel.h>
+#include <kernel/drivers/uart/uart.h>
+#include <kernel/drivers/gpio/gpio.h>
+#include <kernel/drivers/ipc/ipc_fifo.h>
+
+#define PIN_1 PB4
+#define PIN_2 PB3
+#define FIFO_SIZE 2
+#define BLINK_STACK_SIZE 100
+
+/* Control task defines*/
+#define INPUT_BUFF_SIZE 25
+#define CONTROL_STACK_SIZE 200
+#define RECV_TIMEOUT_MS 20
+
+struct blink_msg {
+    uint8_t count;
+    uint8_t blink_freq;
+};
+
+struct blink_msg task_1_buffer[FIFO_SIZE];
+struct blink_msg task_2_buffer[FIFO_SIZE];
+
+ipc_fifo_t<struct blink_msg> fifo1;
+ipc_fifo_t<struct blink_msg> fifo2;
+
+task_data_t blink1;
+task_data_t blink2;
+task_data_t control;
+
+uint8_t blink_stack1[BLINK_STACK_SIZE];
+uint8_t blink_stack2[BLINK_STACK_SIZE];
+uint8_t control_stack[CONTROL_STACK_SIZE];
+
+void blink_task(ipc_fifo_t<struct blink_msg> *fifo, io_port port, uint8_t pin)
+{
+    blink_msg message = {0};
+    uint32_t blink_ms = 0; 
+    uint32_t last_blink_ms = 0;
+    set_gpio_mode(port, pin, OUTPUT);
+
+    while (1) {
+        if (fifo->get_used_size() == 0) {
+            yield();
+            continue;
+        }
+
+        fifo->dequeue(message);
+
+        printf_P(PSTR("%s: blinking %d times at %d freq\n"),
+            &get_current_task()->name[0], message.count, message.blink_freq);
+
+        blink_ms = 1000 / (message.blink_freq * 2) ;
+        message.count *= 2;
+
+        while (message.count != 0) {
+            if (get_ms() - last_blink_ms > blink_ms) {
+                write_gpio(port, pin, read_gpio(port, pin) ^ 1);
+                last_blink_ms = get_ms();
+                message.count -= 1;
+            }
+        }
+    }
+}
+
+void print_help()
+{
+    printf_P(PSTR("Usage\n\
+blink (1 | 2) (count) (frequency) | blink 1 or 2 led count times at frequency\n\
+stats                             | Show scheduler stats\n\
+help                              | Show help message\n"));
+    return;
+}
+
+void print_cmd()
+{
+    task_data_t* task_arr[] = {&blink1, &blink2, &control};
+
+    printf_P(PSTR("\n%-10s| %-5s| %3s| %-9s| %-10s\n"), 
+        "Name", "State", "Pri", "Stack", "Exec (ms)");
+    printf_P(PSTR("----------------------------------------------\n"));
+
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        char state_char;
+        switch (task_arr[i]->state) {
+            case READY: state_char = 'R'; break;
+            case RUNNING: state_char = 'E'; break;
+            case BLOCKED: state_char = 'B'; break;
+            case STOPPED: state_char = 'S'; break;
+            case SLEEP: state_char = 'Z'; break;
+            default: state_char = 'U'; break;
+        }
+
+        printf_P(PSTR("%-10s| %-5c| %3d| %3d /%4d| %10lu\n"),
+            &task_arr[i]->name[0],
+            state_char,
+            task_arr[i]->priority,
+            get_task_stack_usage(task_arr[i]),
+            get_task_stack_size(task_arr[i]),
+            task_arr[i]->exec_time_us / 1000);
+    }
+    printf_P(PSTR("\n"));
+}
+
+void blink_cmd(char *arr, uint16_t size)
+{
+    struct blink_msg msg;
+    bool errno;
+
+    char *token = strtok_P(arr, PSTR(" "));
+    if (token == NULL) return;
+
+    token = strtok_P(NULL, PSTR(" "));
+    if (token == NULL) return;
+    uint8_t id = atoi(token);
+
+    token = strtok_P(NULL, PSTR(" "));
+    if (token == NULL) return;
+    uint8_t count = atoi(token);
+
+    token = strtok_P(NULL, PSTR(" "));
+    if (token == NULL) return;
+    uint16_t freq = atoi(token);
+
+    msg.count = count;
+    msg.blink_freq = freq;
+
+    if (id == 1) {
+        errno = fifo1.enqueue(msg);
+    } else if (id == 2) {
+        errno = fifo2.enqueue(msg);
+    } else {
+        printf_P(PSTR("Invalid ID\n"));
+    }
+
+    if (errno) {
+        printf_P(PSTR("Error: Fifo full\n")); 
+        return;
+    }
+    printf_P(PSTR("Sending command: blink ID: %u COUNT: %u FREQ: %u\n"),
+        id, count, freq);
+}
+
+void control_task(void)
+{
+    char command[INPUT_BUFF_SIZE] = {0};
+    uint32_t last_recv_ms = 0;
+    uint8_t index = 0;
+
+    while (1) 
+    {
+        if (uart_AvailableBytes() == 0) {
+            continue;
+        }
+
+        index = 0;
+        last_recv_ms = get_ms();
+        while (get_ms() - last_recv_ms < RECV_TIMEOUT_MS) {
+            if (uart_AvailableBytes() == 0)
+                continue;
+
+            last_recv_ms = get_ms();
+
+            if (index >= INPUT_BUFF_SIZE - 2) {
+                break;
+            }
+
+            char c = uart_getc();
+
+            if (c != '\n' && c != '\r') {
+                command[index] = c;
+                index += 1;
+            }
+        }
+
+        command[index] = '\0';
+        if (strcmp_P(command, PSTR("stats")) == 0) {
+            print_cmd();
+            continue;
+        }
+
+        if (strncmp_P(command, PSTR("blink"), 5) == 0) {
+            blink_cmd(command, INPUT_BUFF_SIZE);
+            continue;
+        }
+
+        print_help();
+    }
+}
+
+int main(void)
+{
+    kernel_init();
+
+    fifo1.init(task_1_buffer, FIFO_SIZE);
+    fifo2.init(task_2_buffer, FIFO_SIZE);
+
+    create_task(blink1, blink_stack1, BLINK_STACK_SIZE,
+        "blink 1", 1, 1, &blink_task, &fifo1, IO_PORTB, (uint8_t) PIN_1);
+
+    create_task(blink2, blink_stack2, BLINK_STACK_SIZE,
+        "blink 2", 1, 1, &blink_task, &fifo2, IO_PORTB, (uint8_t) PIN_2);
+    
+    create_task(control, control_stack, CONTROL_STACK_SIZE,
+        "control", 1, 1, control_task);
+   
+    sei();
+
+    printf_P(PSTR("starting\n"));
+    uart0_flush();
+    kernel_start();
+    while(1){};
+}
